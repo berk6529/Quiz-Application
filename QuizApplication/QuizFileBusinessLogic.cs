4cf94c8d3d100e3f28dc50ceb155c1ab2b0629b8
LAPTOP-AEKGAMO7\Brad Berkobien <bradberkobien@gmail.com>
2021-06-10 14:20:55 -0400

    Working on the dependency inversion principle

---
+++ QuizApplication/QuizFileBusinessLogic.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuizApplication
+{
+    public class QuizFileBusinessLogic
+    {
+        IQuizFile _quizFileDataAccess;
+
+        public QuizFileBusinessLogic()
+        {
+            _quizFileDataAccess = DataAccessFactory.GetQuizFileDataAccessObj();
+        }
+    }
+}
